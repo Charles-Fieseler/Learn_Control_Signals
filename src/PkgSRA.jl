8a7da7f4e26ab3ab9e450ba61efe168ad1063edb
Charles-Fieseler <konda@uw.edu>
2019-09-24 14:33:57 -0700

    Update main script

--- src/PkgSRA.jl
+++ src/PkgSRA.jl
@@ -1,5 +1,23 @@
 module PkgSRA
 
-greet() = print("Hello World!")
+    ## Base
+    using DiffEqFlux
+    using Flux
+    using OrdinaryDiffEq
+    ## For derivatives
+    # using DSP
+    # using Statistics
+    ## General
+    using Plots
+    # using Random
+
+    include("numerical_derivatives.jl")
+    include("initialize_control_signal.jl")
+    include("dmdc.jl")
+    include("sindyc.jl")
+    include("../utils/array_utils.jl")
+    include("../utils/regression_utils.jl")
+    include("../utils/control_utils.jl")
+    include("forcing_functions.jl")
 
 end # module

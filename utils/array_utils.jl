13c5584b70cd805e549127422801bb626e9c0f96
Charles-Fieseler <konda@uw.edu>
2019-09-30 11:50:46 -0700

    Update array_utils.jl

--- utils/array_utils.jl
+++ utils/array_utils.jl
@@ -11,5 +11,29 @@ function map_rows(f::Function, a::Array)
     return vals
 end
 
+"""
+Returns a function that applies the function-generating function 'f' to
+    the rows of 'a', which can then be called with a SCALAR argument
+"""
+function generate_map_rows_function(f::Function, a::Array)
+    n = size(a,1)
+    func_list = []
+    for i in 1:n
+        push!(func_list, f(a[i,:]))
+    end
+    function F(t)
+        return t .|> func_list
+    end
+    return F
+end
 
-export map_rows
+"""
+Drops dimensions that are equal to one in multidimensional arrays
+    from: https://stackoverflow.com/questions/46289554/drop-julia-array-dimensions-of-length-1
+"""
+function drop_all_1dims(a)
+    return dropdims(a, dims=Tuple(findall(size(a).==1)))
+end
+
+
+export map_rows, generate_map_rows_function, drop_all_1dims

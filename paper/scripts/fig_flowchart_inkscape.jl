fb12b154c0df24dfcb9cd11538bf3c21fcdeed8a
Charles-Fieseler <konda@uw.edu>
2019-10-07 17:03:41 -0700

    Update syntax

--- paper/scripts/fig_flowchart_inkscape.jl
+++ paper/scripts/fig_flowchart_inkscape.jl
@@ -1,4 +1,4 @@
-using PkgSRA, Plots, LinearAlgebra
+using PkgSRA, Plots, LinearAlgebra, Statistics
 using BSON: @load
 pyplot()
 
@@ -29,7 +29,8 @@ this_fig_name = FIGURE_FOLDERNAME*"TMP_fig_flowchart_inkscape_"
 ## 1:  3d example plot: data
 plot_data = plot3d(dat[1, :], dat[2, :], dat[3, :],
         color=COLOR_DICT["true"], lw=2, legend=false,
-        titlefontsize=32)
+        titlefontsize=32,
+        xticks=false, yticks=false, zticks=false,)
         title!("Data")
 
 fname = this_fig_name * "data.png";
@@ -39,10 +40,13 @@ savefig(plot_data, fname)
 ## 2: 3d example plot: uncontrolled
 uncontrolled_ind = 1:200;
     ind = uncontrolled_ind;
-plot_uncontrolled = plot3d(
-        all_vals[1, ind, 1], all_vals[1, ind, 2], all_vals[1, ind, 3],
-        color=COLOR_DICT["model_uncontrolled"], alpha=0.8, lw=2,
-        legend=false)
+let d = sample_trajectory_mean
+        global plot_uncontrolled = plot3d(
+                d[1, ind], d[2, ind], d[3, ind],
+                color=COLOR_DICT["model_uncontrolled"], alpha=0.8, lw=2,
+                xticks=false, yticks=false, zticks=false,
+                legend=false)
+end
     plot3d!(dat[1, ind], dat[2, ind], dat[3, ind],
             color=COLOR_DICT["true"], lw=2)
     title!("Naive Model", titlefontsize=28)
@@ -53,10 +57,10 @@ savefig(plot_uncontrolled, fname)
 
 ## 3: Residual with noise envelope (z coordinate)
 plot_coordinate = 3;
-residual_ind = 1:200;
+residual_ind = 51:250;
     ind = residual_ind;
 plot_residual = plot(residual[plot_coordinate,ind],
-                    ribbon=mean(all_noise), fillalpha=0.5,
+                    ribbon=mean(sample_trajectory_noise), fillalpha=0.5,
                     color=COLOR_DICT["residual"], legend=false,
                     xticks=false, yticks=false, lw=3)
     title!("Residual", titlefontsize=28)
@@ -79,11 +83,14 @@ savefig(plot_ctr_guess, fname)
 ## 5: Controlled model
 controlled_ind = 1:500;
     ind = controlled_ind;
-    ctr_dat = trajectory_samples_ctr[1];
-plot_controlled = plot3d(
-        ctr_dat[1, ind], ctr_dat[2, ind], ctr_dat[3, ind],
-        color=COLOR_DICT["model_controlled"], alpha=0.8, lw=2,
-        legend=false)
+    # ctr_dat = trajectory_samples_ctr[1];
+let d = sample_trajectory_mean_ctr
+        global plot_controlled = plot3d(
+                        d[1, ind], d[2, ind], d[3, ind],
+                        color=COLOR_DICT["model_controlled"], alpha=0.8, lw=2,
+                        xticks=false, yticks=false, zticks=false,
+                        legend=false)
+end
     plot3d!(dat[1, ind], dat[2, ind], dat[3, ind],
             color=COLOR_DICT["true"], lw=2)
     title!("Controlled Model", titlefontsize=28)

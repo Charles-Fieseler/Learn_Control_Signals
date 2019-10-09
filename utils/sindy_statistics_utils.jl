6dfa6c135efcd189409c2af34d507ac06bbecdef
Charles-Fieseler <konda@uw.edu>
2019-10-08 17:35:11 -0700

    Fixing AIC

--- utils/sindy_statistics_utils.jl
+++ utils/sindy_statistics_utils.jl
@@ -1,4 +1,5 @@
 using Distributions
+using StatsBase
 
 #####
 ##### Helper functions
@@ -7,7 +8,7 @@ using Distributions
 Gets number of degrees of freedom, which is equal to the
 number of nonzero terms + 1 for noise
 """
-function dof(m) = length(get_nonzero_terms(m)) + 1
+my_dof(m) = length(get_nonzero_terms(m)) + 1
 
 
 #####
@@ -16,19 +17,19 @@ function dof(m) = length(get_nonzero_terms(m)) + 1
 """
 Akaike Information Criterion (AIC) for SINDy models
 """
-function aic(m::sindyc_model, dat) =
-    -2loglikelihood(MvNormal(), (m(dat) .- dat)') + 2dof(m)
+my_aic(m::sindyc_model, dat, predictor; dist=Normal()) =
+    -2loglikelihood(dist, (m(dat) .- predictor)') + 2my_dof(m)
 
 
 """
 Corrected Akaike Information Criterion.
     Used for small sample sizes (Hurvich and Tsai 1989)
 """
-function aicc(m::sindyc_model, dat)
-    k = dof(m)
+function my_aicc(m::sindyc_model, dat, predictor; dist=Normal())
+    k = my_dof(m)
     n = length(dat) # equal to numel(dat)
     correction = 2k*(k+1)/(n-k-1)
-    return aic(m, dat) + correction
+    return my_aic(m, dat, predictor, dist=dist) + correction
 end
 
 
@@ -39,23 +40,35 @@ end
 Loop over several values of sparsity and choose the best model via
     MINIMIZING the 'selection_criterion' function
         selection_criterion() should have signature:
-        selection_criterion(model, data)
+        selection_criterion(model, data, predictor)
 
     Also returns all the sparsity values and all the models
 """
-function sindyc_ensemble(X, X_grad, library, quantile_list;
-                selection_criterion=aic)
-    n = length(quantile_list)
+function sindyc_ensemble(X, X_grad, library, val_list;
+                selection_criterion=my_aic,
+                selection_dist=Normal(),
+                sparsification_mode="quantile")
+    n = length(val_list)
     all_models = Vector(undef, n)
     all_criteria = zeros(n)
     # Convinience function
-    make_model(x) = sindyc(X, X_grad; library=library,
-                            use_lasso=true,
-                            quantile_threshold=x)
+    make_model(x) = if sparsification_mode == "quantile"
+        sindyc(X, X_grad; library=library,
+                                use_lasso=true,
+                                quantile_threshold=x)
+    elseif sparsification_mode == "num_terms"
+        sindyc(X, X_grad; library=library,
+                                use_lasso=true,
+                                quantile_threshold=nothing,
+                                num_terms=x)
+    else
+        error("Unknown sparsification mode")
+    end
 
-    for (i, val) in enumerate(quantile_list)
+    for (i, val) in enumerate(val_list)
         m = make_model(val)
-        all_criteria[i] = selection_criterion(m, X)
+        all_criteria[i] = selection_criterion(m,
+                X, X_grad, dist=selection_dist)
         all_models[i] = m
     end
 
@@ -70,4 +83,4 @@ end
 
 #
 export sindyc_ensemble,
-        aic, aicc, dof
+        my_aic, my_aicc, my_dof

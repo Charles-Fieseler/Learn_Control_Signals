2b3de2a049e20cb4fbb0fbf22c9e0c7c6676e509
Charles-Fieseler <konda@uw.edu>
2020-01-14 09:53:47 -0800

    Finish SRA iteration loop

--- paper/dat/dat_library_of_examples_new.jl
+++ paper/dat/dat_library_of_examples_new.jl
@@ -33,3 +33,32 @@ sol = solve_sir_system(U_func_time=my_U_func_time2)
 dat = Array(sol)
 numerical_grad = numerical_derivative(dat, ts)
 true_grad = core_dyn_true(dat)
+
+U_true = zeros(size(dat))
+for (i, t) in enumerate(ts)
+    U_true[:,i] = my_U_func_time2(t)
+end
+
+# Intialize truth object
+this_truth = sra_truth_object(true_grad, U_true, core_dyn_true)
+
+
+#####
+##### Build SRA object and analyze
+#####
+# Initialize
+this_model = sra_stateful_object(ts, tspan, dat, u0, numerical_grad)
+fit_first_model(this_model, 100);
+
+
+### Iterate
+calculate_subsampled_ind(this_model);
+fit_model(this_model);
+
+# println("Iteration $i")
+print_current_equations(this_model, digits=5)
+print_true_equations(this_truth, digits=5)
+
+plot_subsampled_points(this_model)
+plot_subsampled_simulation(this_model)
+plot_residual(this_model)

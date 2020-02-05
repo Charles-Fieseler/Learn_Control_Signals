c823871368d3bdacafd18fb43611dd3b8c06aca0
Charles-Fieseler <konda@uw.edu>
2020-02-05 13:49:39 -0800

    Update sindy_test.jl

--- test/sindy_test.jl
+++ test/sindy_test.jl
@@ -16,30 +16,42 @@ test_model = sindyc(dat, numerical_grad, nothing, ts,
 
 # Built correct terms: names
 named_terms = build_term_names(test_model)
-@test named_terms ==
-    ["x", "y", "z", "", "xx", "xy", "xz", "yy", "yz", "zz"]
+@testset "Term Metadata" begin
+    @test named_terms ==
+        ["x", "y", "z", "", "xx", "xy", "xz", "yy", "yz", "zz"]
+end
 
 # Built correct terms: values
 library_functions = convert_string2function(test_model.library)
 terms = calc_augmented_data(dat, library_functions)
 
-@test size(terms,1) == length(named_terms)
 
 @testset "Data Augmentation" begin
+    @test size(terms,1) == length(named_terms)
     @test terms[1:3,:] == dat
     @test terms[4,:] == ones(size(dat,2))
     @test terms[5:10,:] == calc_cross_terms(dat, 2)
 end;
 
-# Type tests
-@test typeof(test_model) <: sindyc_model
-
-# Nonzero term tests
+# Nonzero term and type tests
 t1 = get_nonzero_terms(test_model)
 t2 = get_nonzero_terms(core_dyn_true)
 
-@test issubset(t2, t1)
+@testset "Type Tests" begin
+    @test typeof(test_model) <: sindyc_model
+    @test issubset(t2, t1)
+end
 
+# Also test cross validation function
+null_model = sindyc(dat, 0.1.*randn(size(dat)), nothing, ts,
+                        library=sindy_library,
+                        use_lasso=true)
+err_test = sindy_cross_validate(test_model, dat, numerical_grad)
+err_null = sindy_cross_validate(null_model, dat, numerical_grad)
+
+@testset "Cross Validation" begin
+    @test err_test < err_null
+end
 
 
 # SINDY tests: up to cubed terms
@@ -58,5 +70,7 @@ test_model2 = sindyc(dat, numerical_grad, nothing, ts,
                         var_names=["x", "y"])
 
 named_terms = build_term_names(test_model2)
-@test named_terms ==
-    ["x", "y", "", "xx", "xy", "yy", "xxx", "xxy", "xyy", "yyy"]
+@testset "Cubic term names" begin
+    @test named_terms ==
+        ["x", "y", "", "xx", "xy", "yy", "xxx", "xxy", "xyy", "yyy"]
+end

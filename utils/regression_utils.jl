2feaf6c3e172844591c216690008909f0e3d44e0
Charles-Fieseler <konda@uw.edu>
2020-02-05 16:54:35 -0800

    Refactor into optimizer-object interface

--- utils/regression_utils.jl
+++ utils/regression_utils.jl
@@ -47,59 +47,61 @@ function threshold_signal!(U_guess; hard_threshold=1e-5)
     end
 end
 
-"""
-Simple implementation of sequential least squares thresholding for a linear
-    regression model
-"""
-function sparse_regression(X::Matrix, y::Matrix;
-                           num_iter=2,
-                           quantile_threshold=0.1,
-                           hard_threshold=nothing,
-                           num_terms=nothing)
-    if size(y,1) > 1
-        A = zeros(size(y,1), size(X,1))
-        # For the 2d predictor case, just loop over rows
-        for i in 1:size(y,1)
-            if num_terms !== nothing
-                # Constrain the number of terms row by row
-                if length(num_terms)>1
-                    n = num_terms[i]
-                else
-                    n = num_terms
-                end
-            else
-                n = nothing
-            end
-            A[i, :] = sparse_regression(X, y[i:i,:],
-                            num_iter=num_iter,
-                            quantile_threshold=quantile_threshold,
-                            hard_threshold=hard_threshold,
-                            num_terms=n)
-        end
-        return A
-    end
-    # If this is only one row, initialize with L2
-    A = y/X
-    # sparsify_signal!(A, quantile_threshold=quantile_threshold)
-    # Redo, removing the rows in X that were zeroed out
-    for i in 1:num_iter
-        if hard_threshold == nothing && num_terms == nothing
-            sparsify_signal!(A, quantile_threshold=quantile_threshold)
-        elseif num_terms == nothing
-            threshold_signal!(A, hard_threshold=hard_threshold)
-        else
-            A = keep_n_terms(A, num_terms)
-        end
-        which_rows = vec(abs.(A) .> 0)
-        A_subset = y/(X[which_rows, :])
-        A[which_rows] = A_subset
-        A[.!which_rows] .= 0.0 # Retain the previous zeros
-    end
+# """
+# Simple implementation of sequential least squares thresholding for a linear
+#     regression model
+# """
+# function sparse_regression(X::Matrix, y::Matrix;
+#                            num_iter=2,
+#                            quantile_threshold=0.1,
+#                            hard_threshold=nothing,
+#                            num_terms=nothing)
+#     if size(y,1) > 1
+#         A = zeros(size(y,1), size(X,1))
+#         # For the 2d predictor case, just loop over rows
+#         for i in 1:size(y,1)
+#             if num_terms !== nothing
+#                 # Constrain the number of terms row by row
+#                 if length(num_terms)>1
+#                     n = num_terms[i]
+#                 else
+#                     n = num_terms
+#                 end
+#             else
+#                 n = nothing
+#             end
+#             A[i, :] = sparse_regression(X, y[i:i,:],
+#                             num_iter=num_iter,
+#                             quantile_threshold=quantile_threshold,
+#                             hard_threshold=hard_threshold,
+#                             num_terms=n)
+#         end
+#         return A
+#     end
+#     # If this is only one row, initialize with L2
+#     A = y/X
+#     # sparsify_signal!(A, quantile_threshold=quantile_threshold)
+#     # Redo, removing the rows in X that were zeroed out
+#     for i in 1:num_iter
+#         if hard_threshold == nothing && num_terms == nothing
+#             sparsify_signal!(A, quantile_threshold=quantile_threshold)
+#         elseif num_terms == nothing
+#             threshold_signal!(A, hard_threshold=hard_threshold)
+#         else
+#             A = keep_n_terms(A, num_terms)
+#         end
+#         which_rows = vec(abs.(A) .> 0)
+#         A_subset = y/(X[which_rows, :])
+#         A[which_rows] = A_subset
+#         A[.!which_rows] .= 0.0 # Retain the previous zeros
+#     end
+#
+#     return A
+# end
 
-    return A
-end
 
 
-
-export sparse_regression, sparsify_signal, threshold_signal!,
+# export sparse_regression, sparsify_signal, threshold_signal!,
+#         keep_n_terms
+export sparsify_signal, threshold_signal!,
         keep_n_terms
---
+++ utils/sparse_regression_functions.jl
@@ -0,0 +1,152 @@
+
+###
+### Structs that define the solver settings
+###
+abstract type sparse_solver end
+
+# Sequential Least Squares methods
+struct slst_hard <: sparse_solver
+    num_iter::Number
+    hard_threshold::Number
+end
+
+struct slst_quantile <: sparse_solver
+    num_iter::Number
+    quantile_threshold::Number
+end
+
+struct slst_number <: sparse_solver
+    num_iter::Number
+    num_terms::Vector
+end
+
+struct dense_solver <: sparse_solver end
+
+###
+### Actually solving
+###
+"""
+Uses sparse_solver objects and implements Sequential Least Squares Thresholding
+    in various ways
+
+This is the outer user-facing function that performs checks, namely whether
+    recursion over rows is needed
+"""
+function sparse_regression(alg::sparse_solver, X::Matrix, y::Matrix)
+                           # num_iter=2,
+                           # quantile_threshold=0.1,
+                           # hard_threshold=nothing,
+                           # num_terms=nothing)
+    # Initial check: should we do recursive?
+    if size(y,1) > 1
+        A = zeros(size(y,1), size(X,1))
+        # For the 2d predictor case, just loop over rows
+        for i in 1:size(y,1)
+            # if num_terms !== nothing
+            #     # Constrain the number of terms row by row
+            #     if length(num_terms)>1
+            #         n = num_terms[i]
+            #     else
+            #         n = num_terms
+            #     end
+            # else
+            #     n = nothing
+            # end
+            # Call inner function
+            this_y = y[i:i,:]
+            A[i, :] = _sparse_regression(alg, X, this_y, i)#,
+                            # num_iter=num_iter,
+                            # quantile_threshold=quantile_threshold,
+                            # hard_threshold=hard_threshold,
+                            # num_terms=n)
+        end
+        return A
+    end
+
+    A =  _sparse_regression(alg, X, y, nothing)
+    return A
+end
+
+
+
+###
+### Private functions
+###
+"""
+Fake "sparse" solver; just L2 solution
+"""
+function _sparse_regression(alg::dense_solver, X::Matrix, y::Matrix,
+                            which_data_row::Number, A=nothing)
+    return X/y
+end
+
+"""
+Implements a hard threshold
+
+see also: threshold_signal!
+"""
+function _sparse_regression(alg::slst_hard, X::Matrix, y::Matrix,
+                            which_data_row::Number, A=nothing)
+    if A == nothing
+        A = X/y
+    end
+    # Redo, removing the rows in X that were zeroed out
+    for i in 1:alg.num_iter
+        threshold_signal!(A, hard_threshold=alg.hard_threshold)
+        which_rows = vec(abs.(A) .> 0)
+        A_subset = y/(X[which_rows, :])
+        A[which_rows] = A_subset
+        A[.!which_rows] .= 0.0 # Retain the previous zeros
+    end
+
+    return A
+end
+
+
+"""
+Implements a quantile threshold
+
+see also: sparsify_signal!
+"""
+function _sparse_regression(alg::slst_quantile, X::Matrix, y::Matrix,
+                            which_data_row::Number, A=nothing)
+    if A == nothing
+        A = X/y
+    end
+    # Redo, removing the rows in X that were zeroed out
+    for i in 1:alg.num_iter
+        sparsify_signal!(A, quantile_threshold=alg.quantile_threshold)
+        which_rows = vec(abs.(A) .> 0)
+        A_subset = y/(X[which_rows, :])
+        A[which_rows] = A_subset
+        A[.!which_rows] .= 0.0 # Retain the previous zeros
+    end
+
+    return A
+end
+
+"""
+Implements a hard cap on the number of terms allowed
+
+see also: keep_n_terms
+"""
+function _sparse_regression(alg::slst_number, X::Matrix, y::Matrix,
+                            which_data_row::Number, A=nothing)
+    if A == nothing
+        A = X/y
+    end
+    # Redo, removing the rows in X that were zeroed out
+    for i in 1:alg.num_iter
+        A = keep_n_terms(A, alg.num_terms[which_data_row])
+        which_rows = vec(abs.(A) .> 0)
+        A_subset = y/(X[which_rows, :])
+        A[which_rows] = A_subset
+        A[.!which_rows] .= 0.0 # Retain the previous zeros
+    end
+
+    return A
+end
+
+
+export slst_hard, slst_quantile, slst_number, dense_solver,
+    sparse_regression

c409f65cc3c2c3c1ab6a9d81a86cfd46b1267065
Charles-Fieseler <konda@uw.edu>
2020-02-07 11:03:31 -0800

    Update dat_library_of_examples_lv_noise.jl

--- paper/dat/dat_library_of_examples_lv_noise.jl
+++ paper/dat/dat_library_of_examples_lv_noise.jl
@@ -1,6 +1,6 @@
 using PkgSRA
 using Plots, Random, Distributions, Interpolations
-using StatsPlots
+using StatsPlots, StatsBase
 pyplot()
 Random.seed!(11)
 using BSON: @save
@@ -50,18 +50,20 @@ this_truth = sra_truth_object(dat_raw, true_grad, U_true, core_dyn_true)
 ##### Loop over additive noise
 #####
 
-noise_vals = [0, 0]
+# noise_vals = [0, 1e-2, 0.1]
+noise_vals = 0:0.05:0.3
 # noise_factor = norm(numerical_grad)
 # noise_factor = 1;
 # noise_vals .*= noise_factor
 # noise_factor = sqrt.(sum(numerical_grad.^2, dims=2))
-num_models = 5
+num_models = 20
 all_err = zeros(length(noise_vals), num_models)
+all_i = zeros(length(noise_vals), num_models)
 
 # Make model template
 variable_names = ["x", "y"]
 optimizer = slstNumber((1, 1));
-library = Dict("cross_terms"=>[2])
+library = Dict("cross_terms"=>[2], "constant"=>nothing)
 model_template = sindyModel(convert_string2function(library),
     variable_names, optimizer)
 
@@ -69,7 +71,7 @@ for (i,σ) in enumerate(noise_vals)
     for j in 1:num_models
         noisy_grad = numerical_grad .+ σ.*randn(size(dat))
         # Initialize
-        this_model = sra_stateful_object(ts, tspan, dat, u0, noisy_grad)
+        global this_model = sra_stateful_object(ts, tspan, dat, u0, noisy_grad)
         # Reset parameters because we are NOT using control
         this_model.parameters = get_sra_defaults(false)
         prams = this_model.parameters
@@ -97,6 +99,7 @@ for (i,σ) in enumerate(noise_vals)
         println("Finished with $(this_model.i) iterations")
 
         all_err[i, j] = calc_coefficient_error(this_model, this_truth)
+        all_i[i, j] = this_model.i
         println("Final error = $(all_err[i, j])")
         println("")
     end
@@ -104,7 +107,7 @@ for (i,σ) in enumerate(noise_vals)
     println("=====================================")
 end
 
-# boxplot(noise_vals, all_err')
+# boxplot(collect(noise_vals)', all_err')
 
 vec_err, std_err = mean_and_std(all_err, 2)
 # vec_err, std_err = mean_and_std(all_err)
@@ -112,6 +115,11 @@ plot(noise_vals, vec_err, ribbon=std_err)
     xlabel!("Noise")
     ylabel!("Coefficient Error")
 
+vec_i, std_i = mean_and_std(all_i, 2)
+# vec_err, std_err = mean_and_std(all_err)
+plot(noise_vals, vec_i, ribbon=std_err)
+    xlabel!("Noise")
+    ylabel!("Number of iterations")
 # plot_subsampled_points(this_model)
 # plot_subsampled_simulation(this_model, 2)
 # plot_subsampled_derivatives(this_model, 2)

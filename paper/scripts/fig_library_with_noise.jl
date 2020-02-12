d9925cd31a5d1f32be72488f1d2f2a9c23f6c984
Charles-Fieseler <konda@uw.edu>
2020-02-12 10:16:27 -0800

    Update fig_library_with_noise.jl

--- paper/scripts/fig_library_with_noise.jl
+++ paper/scripts/fig_library_with_noise.jl
@@ -9,10 +9,21 @@ include("paper_settings.jl")
 #####
 ##### Plot AND save using external function
 #####
+
+## Lotka Volterra
 this_dat_name = "dat_library_with_noise_lv_"
 plot_final = plot_library_noise(this_dat_name, "Lotka Volterra";
                         plot_opt=plot_opt)
 
-this_fig_name = FIGURE_FOLDERNAME*"fig_library_with_noise";
+this_fig_name = FIGURE_FOLDERNAME*"fig_library_with_noise_";
 fname = this_fig_name * "lotkaVolterra.png";
+savefig(plot_final, fname)
+
+## FitzHugh Nagumo
+this_dat_name = "dat_library_with_noise_fhn_"
+plot_final = plot_library_noise(this_dat_name, "FitzHugh Nagumo";
+                        plot_opt=plot_opt)
+
+this_fig_name = FIGURE_FOLDERNAME*"fig_library_with_noise_";
+fname = this_fig_name * "fitzHughNagumo.png";
 savefig(plot_final, fname)
